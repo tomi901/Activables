2e353bdae76e52af56acb243848c6013941c13eb
Tomás Rinaldi <tomirinaldi8@gmail.com>
2018-12-28 13:29:08 -0300

    Initial commit

---
+++ Activable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+public abstract class Activable : MonoBehaviour
+{
+
+    // Leave this instead of using auto-property to be able to see in the inspector in debug mode
+    private bool activated = false;
+    /// <summary>
+    /// The actual state of the this <see cref="Activable"/>, if set, it doesn't call the virtual method
+    /// <see cref="OnActivatedStateChange(bool)"/>.
+    /// </summary>
+    public bool Activated { get { return activated; } protected set { activated = value; } }
+
+    /// <summary>
+    /// Sets the actual state of this <see cref="Activable"/> with and calls the 
+    /// <see cref="OnActivatedStateChange(bool)"/> virtual method if the active state changes.
+    /// </summary>
+    /// <param name="active">The state to set.</param>
+    protected void SetActivated(bool active)
+    {
+        if (activated == active) return;
+
+        activated = active;
+        OnActivatedStateChange(active);
+    }
+
+    protected void ToggleState()
+    {
+        SetActivated(!activated);
+    }
+
+    protected abstract void OnActivatedStateChange(bool active);
+
+    public static void GizmosDrawSwitchActuatorConnection(Switch @switch, ActivableActuator actuator)
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(@switch.transform.position, actuator.transform.position);
+    }
+
+}
---
+++ CompositeSwitch.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// A switch that requires all the switchs to be active for this object to be set active.
+/// </summary>
+public class CompositeSwitch : Switch, IEnumerable<Switch>
+{
+
+    [SerializeField]
+    private List<Switch> switches = new List<Switch>();
+
+    private void Awake()
+    {
+        foreach (Switch switchObject in this)
+        {
+            switchObject.OnActivateStateChange += OnSubswitchActivatedChange;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        foreach (Switch switchObject in this)
+        {
+            switchObject.OnActivateStateChange -= OnSubswitchActivatedChange;
+        }
+    }
+
+    private void OnSubswitchActivatedChange(bool subswitchStateChange)
+    {
+        SetActivated(switches.All(s => s.Activated));
+    }
+
+    protected override void OnDrawGizmosSelected()
+    {
+        base.OnDrawGizmosSelected();
+        switches.ForEach(s => GizmosSwitchDrawKeyConnection(s));
+    }
+
+    // IEnumerable interface
+
+    public IEnumerator<Switch> GetEnumerator()
+    {
+        return switches.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return switches.GetEnumerator();
+    }
+
+}
